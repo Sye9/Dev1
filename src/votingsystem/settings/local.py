5705328b57e79133e01059c70b7b55ce004030c1
dagusen <louisaleksieb.dagusen@gmail.com>
2018-02-16 17:47:27 +0800

    setup static files

--- src/votingsystem/settings/local.py
+++ src/votingsystem/settings/local.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'votingsystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [[os.path.join(BASE_DIR, "templates")]],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
